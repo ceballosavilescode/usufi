de733ce90983b0cad938df81a6cfbe3c452e2b76
ceballosavilescode <ceballosavilescode@gmail.com>
2025-08-31 23:21:09 -0400

    Update usufi.py

--- usufi.py
+++ usufi.py
@@ -219,7 +219,7 @@ def tabla_dinamica():
 
 class Usufi(object):
 	usufi = pd.DataFrame()
-	ruta_archivos = os.path.join(os.getcwd(), "*.csv")
+	ruta_archivos = os.path.join(os.getcwd(), "*")
 	print(ruta_archivos) 
 	time.sleep(5)
 	print("estoy buscando el archivo...!")
